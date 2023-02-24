84aebaa1e9dd64d0455d4f0477bbfdaeca4ef7bd
FATEH Nacer <nacer.fateh@etu.univ-amu.fr>
2023-02-24 15:37:09 +0100

    Deleting the scripts folder

--- scripts/page-redirect.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('error', function (e) {
-    if (e && e.status === 404) {
-        window.location.href = '/html/page-redirect.html';
-    }
-});
-
-console.log('page-redirect.js loaded');
